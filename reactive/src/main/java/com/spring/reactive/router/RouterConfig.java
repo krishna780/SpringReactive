6b45b292e696c831332f3c3c58100e79c8a1408a
krisna kanth <kkanth2904@menard-inc.com>
2022-09-03 12:31:47 -0400

    reactive"

--- reactive/src/main/java/com/spring/reactive/router/RouterConfig.java
+++ reactive/src/main/java/com/spring/reactive/router/RouterConfig.java
@@ -5,19 +5,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class RouterConfig {
     @Autowired
     private CustomerHandler handler;
+
+    @Autowired
+    SecurityManager securityManager;
     @Bean
     public RouterFunction<ServerResponse> routerFunction(){
-        return RouterFunctions.route()
+        return route()
                 .GET("/route/customers",handler::loadCustomers)
                 .GET("/route/customer/{input}",handler::findCustomer)
                 .POST("/router/customer/save",handler::saveCustomer)
                 .build();
     }
+
+      @Bean
+      public  RouterFunction<ServerResponse> routerFunc(){
+        return route().path(
+                "/route/customers", b1->b1.GET(handler::loadCustomers)
+                        .GET("/{input}",handler::findCustomer)
+                .before(r-> ServerRequest.from(r).header("X-RequestHeader","Value")
+                        .build()))
+                .POST("save",handler::loadCustomers)
+                .build();
+      }
+@Bean
+      public RouterFunction<ServerResponse> responseRouterFunction(){
+        return route().path("/route/customers",
+                   b1->b1.nest(accept(APPLICATION_JSON),b2->b2
+                           .GET(handler::loadCustomers)
+                           .GET("/{input}",handler::findCustomer))
+                           .POST("/{save}",handler::saveCustomer))
+                .build();
+      }
+
 }
---
+++ reactive/src/main/java/com/spring/reactive/router/WebClientEx.java
@@ -0,0 +1,39 @@
+package com.spring.reactive.router;
+
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+
+@Configuration
+public class WebClientEx {
+    @Bean
+    public WebClient webClient(){
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient()))
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .defaultCookie("cookieKey", "cookieVal")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE,
+                        MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+    private HttpClient httpClient() {
+        return HttpClient.create().option(
+                ChannelOption.CONNECT_TIMEOUT_MILLIS,5000)
+                .responseTimeout(Duration.ofMillis(5000))
+                .doOnConnected(connection -> connection.addHandlerLast(
+                        new ReadTimeoutHandler(5000, TimeUnit.MICROSECONDS))
+                        .addHandlerLast(new WriteTimeoutHandler(5000,TimeUnit.MILLISECONDS)));
+    }
+}
